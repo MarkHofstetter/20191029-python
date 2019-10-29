ee37e27d7f60df851b099ab98e2dc49407e03993
Mark Hofstetter <mark@hofstetter.at>
2019-10-29 10:48:30 +0100

    initial

---
+++ 001-variablen.py
@@ -0,0 +1,40 @@
+a = 5
+b = 'Hallo'
+c = "Welt"
+
+print(b + ' ' + c + str(a))
+
+print(b, a)
+# print "Hallo" # funktioniert nur in python2
+
+# Kommentar
+t = True   
+f = False
+
+n = None
+
+print(t, type(t))
+
+z = "5"
+
+zi = int(z)
+
+print(hex(id(zi)))
+print(hex(id(a)))
+
+a = 6
+print(hex(id(a)))
+
+print(a + int(z))  # 10
+print(str(a) + z)  # "55"
+
+
+
+
+
+#  print(x)
+'''
+mehr
+zeiliger 
+Kommentar
+'''
