927f9eb9203aa002a31fca110ab94b1e410dae71
Mark Hofstetter <mark@hofstetter.at>
2019-10-29 15:57:52 +0100

    fibonaci

---
+++ 007-lists.py
@@ -0,0 +1,40 @@
+'''
+array, feld, 
+
+list
+'''
+
+def get_add_elements2fibonaci(fib_number_list, number_of_elements2add = 1):
+    fib = fib_number_list[-2:] 
+    for i in range(number_of_elements2add):
+        fib.append(fib[-1] + fib[-2])
+    return fib[2:]  
+   
+fib = [1,
+       1,
+       2,
+       3,
+       5,
+       8,
+       13,       
+       ]
+       
+print(fib)
+for i, element in enumerate(fib):
+    print(i, element)
+
+a = 2
+
+for i in get_add_elements2fibonaci(
+             fib_number_list = fib, 
+             number_of_elements2add = 3):
+    fib.append(i)                
+
+
+print(fib)
+
+
+
+
+
+
