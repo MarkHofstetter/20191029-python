87759cb808c7f59ce23d8d6f1ce41996f05d025e
Mark Hofstetter <mark@hofstetter.at>
2019-11-05 12:53:08 +0100

    pandas demo

--- 014-csv/use-pandas.py
+++ 014-csv/use-pandas.py
@@ -6,6 +6,13 @@ from matplotlib import pyplot as plt
 data = pd.read_csv('data_clean.csv', encoding='latin1')
 
 print(data)
-data.plot(kind='bar', x=2, y=1)
-plt.show()
+# data.plot(kind='bar', x=2, y=1)
+# plt.show()
 
+print(data['2004'])
+
+print(data.iloc[1])
+
+# print(data.iloc[1].mean())
+data['mean'] = data.mean(axis = 1)
+print(data)
--- 015-pandas/read_housing.py
+++ 015-pandas/read_housing.py
@@ -9,6 +9,8 @@ print(data.head())
 
 # data.info()
 print(data.describe())
+print(data['population'].mean())
 
-data.hist(bins=50, figsize=(20,15))
-plt.show()
+print(data['ocean_proximity'].value_counts())
+# data.hist(bins=50, figsize=(20,15))
+# plt.show()
